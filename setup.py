1ea34c30a264f4fd1c4e30aaff4fe5cad3ba945a
Ethan Furman <ethan@stoneleaf.us>
2016-04-13 23:15:18 -0700

    add short long description

--- setup.py
+++ setup.py
@@ -7,7 +7,45 @@ if sys.version_info[:2] < (2, 7):
 else:
     required = []
 
-long_desc = open('enum/doc/enum.rst').read()
+long_desc = '''\
+enum --- support for enumerations
+========================================
+
+An enumeration is a set of symbolic names (members) bound to unique, constant
+values.  Within an enumeration, the members can be compared by identity, and
+the enumeration itself can be iterated over.
+
+    from enum import Enum
+
+    class Fruit(Enum):
+        apple = 1
+        banana = 2
+        orange = 3
+
+    list(Fruit)
+    # [<Fruit.apple: 1>, <Fruit.banana: 2>, <Fruit.orange: 3>]
+
+    len(Fruit)
+    # 3
+
+    Fruit.banana
+    # <Fruit.banana: 2>
+
+    Fruit['banana']
+    # <Fruit.banana: 2>
+
+    Fruit(2)
+    # <Fruit.banana: 2>
+
+    Fruit.banana is Fruit['banana'] is Fruit(2)
+    # True
+
+    Fruit.banana.name
+    # 'banana'
+
+    Fruit.banana.value
+    # 2
+'''
 
 setup( name='enum34',
        version='1.1.1',
